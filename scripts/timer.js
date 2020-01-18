c214e48112ee2fa4b5b61dd18dd6a50e11b9883e
Charlotte Hughes <hues.charlotte@gmail.com>
2020-01-18 14:17:13 +0000

    added countdown to the ui and disabled arrow buttons when timer is at limit

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,14 +1,14 @@
 let sessionTime = {
     name: 'work',
-	minute: 0,
-	second: 4,
+	minute: 25,
+	second: 0,
 	active: true
 }
 
 let breakTime = {
     name: 'break',
-	minute: 0,
-	second: 2,
+	minute: 5,
+	second: 0,
 	active: false
 }
 
@@ -18,14 +18,15 @@ let isPaused = true;
 let id;
 
 function checkActive() {
-	clearInterval(id);
+    clearInterval(id);
+    currentMinute = sessionTime.minute;
+    currentSecond = sessionTime.second;
 	sessionTime.active === true ? switchTimer(breakTime, sessionTime) : switchTimer(sessionTime, breakTime);
 }
 
 function switchTimer(active, inactive) {
-    console.log(active.name);
 	currentMinute = active.minute;
-	currentSecond = active.second;
+    currentSecond = active.second;
 	active.active = true;
 	inactive.active = false;
 	id = setInterval(startTimer, 1000);
@@ -39,6 +40,7 @@ function startTimer() {
         currentSecond = 59;
     } else if (currentSecond > 0) { currentSecond -= 1; }
     else { checkActive(); }
+    timerDisplay.innerHTML = currentMinute + ":" + currentSecond;
 }
 
 function pause() {
@@ -66,4 +68,49 @@ const pauseButton = document.getElementById('pause-button');
 
 playButton.addEventListener('click', play);
 pauseButton.addEventListener('click', pause);
+
+const workUp = document.querySelector('.session .up');
+const workDown = document.querySelector('.session .down');
+const breakUp = document.querySelector('.break .up');
+const breakDown = document.querySelector('.break .down');
+const timeAdjusters = document.querySelectorAll('.times button');
+timeAdjusters.forEach(button => button.addEventListener('click', adjustTime))
+
+
+const workTimeDisplay = document.querySelector('.session h2');
+const breakTimeDisplay = document.querySelector('.break h2');
+const timerDisplay = document.querySelector('#time-left h1');
+breakTimeDisplay.innerHTML = breakTime.minute;
+workTimeDisplay.innerHTML = sessionTime.minute;
+timerDisplay.innerHTML = currentMinute + ":" + currentSecond;
+
+
+
+function adjustTime() {
+    switch(this.dataset.parent + this.classList) {
+        case 'breakup':
+            breakTime.minute += 1;;
+            break;
+        case 'breakdown':
+            if (breakTime.minute === 1) {
+                break;
+            }  else {
+                breakTime.minute -= 1;
+                break;
+            }      
+        case 'sessionup':
+            sessionTime.minute += 1;
+            break;
+        case 'sessiondown':
+            sessionTime.minute -= 1;
+    }
+    breakTimeDisplay.innerHTML = breakTime.minute;
+    workTimeDisplay.innerHTML = sessionTime.minute; 
+    sessionTime.minute <= 1 ? workDown.classList.add('disabled') : workDown.classList.remove('disabled');
+    breakTime.minute <= 1 ? breakDown.classList.add('disabled') : breakDown.classList.remove('disabled');
+    sessionTime.minute >= 60 ? workUp.classList.add('disabled') : workUp.classList.remove('disabled');
+    breakTime.minute >= 60 ? breakUp.classList.add('disabled') : breakUp.classList.remove('disabled');
+
+    timerDisplay.innerHTML = sessionTime.minute + ":" + sessionTime.second;
+}
 
